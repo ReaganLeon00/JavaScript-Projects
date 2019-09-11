8f0453c4e8e3fe35fb0c9fca0d9c476c49ea24bc
ReaganLeon00 <maggie.leon00@gmail.com>
2019-09-11 12:27:11 -0600

    Add code and files
    Add code and files

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,15 @@
+window.alert("Hello, World!");
+document.write("Hello, World!");
+var A = " This is a string.";
+document.write(A);
+var B =" Concatenated " + " String "
+document.write(B)
+var Family = " The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+    Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+
+function myFirstFunction() {
+    var str= "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text") .innerHTML = result;
+}
