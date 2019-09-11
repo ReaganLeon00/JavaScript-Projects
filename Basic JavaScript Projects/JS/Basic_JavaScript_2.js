c5032a3861372330adf0000f959bc03601ecc351
ReaganLeon00 <maggie.leon00@gmail.com>
2019-09-11 13:13:04 -0600

    Add file
    Add file

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,10 @@
+function myFirstFunction() {
+    var str= "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text") .innerHTML = result;
+}
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
