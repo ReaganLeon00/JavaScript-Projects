29e4c995a5d8903c677645f05e5923bb19c4b752
ReaganLeon00 <maggie.leon00@gmail.com>
2019-09-13 10:48:22 -0600

    Add code
    Add code

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,48 @@
+
+function my_Function() {
+    document.getElementById("Test").innerHTML= 2E310;
+}
+document.write(2E310);
+
+document.write(-3E310)
+
+document.write(10>2);
+
+document.write(10<2);
+
+console.log(2+2);
+
+document.write("10"+5);
+ 
+console.log(10<2);
+
+document.write(10==10);
+document.write(3==11)
+
+Date: 11/05/2120;
+Date: 11/05/2120;
+document.write(Date===Date)
+
+X= 82;
+Y = "82";
+document.write(X===Y)
+
+document.write(5>2 && 10>4);
+
+document.write(5>10 && 10>4);
+
+document.write(5>10 || 10>4);
+
+document.write(5>10 || 10>20);
+
+
+
+
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(5>10);
+}
+
+function not_Function() { 
+    document.getElementById("Not").innerHTML = !(20>10);
+}
